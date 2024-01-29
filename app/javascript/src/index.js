5dab7c54b06861a8fe7dd7e829bf0222784d4927
h8rry <harrychenca@gmail.com>
2024-01-29 20:43:37 +0800

    Fixes

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -4,12 +4,18 @@ import {
   postTask,
 } from "./requests.js";
 
-indexTasks(function (response) {
-  var htmlString = response.tasks.map(function(task) {
-    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-      " + task.content + "\
-      </div>";
+function renderAllTasks() {
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
+        " + task.content + "\
+        </div>";
+    });
+  
+    $("#tasks").html(htmlString);
   });
+};
 
-  $("#tasks").html(htmlString);
+$(document).ready(function() {
+  renderAllTasks();
 });
